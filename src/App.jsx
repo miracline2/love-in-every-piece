fa40f2c99529523c024e04e57c369a471b5937ca
miracline2 <francesca4518@gmail.com>
2026-02-10 22:34:26 +0530

    App.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const [showTryAgain, setShowTryAgain] = useState(false)
   const [checkTimeout, setCheckTimeout] = useState(null)
 
-  
+  ''
   useEffect(() => {
 
     if (checkTimeout) {
